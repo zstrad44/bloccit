32dbbdb55395620440b8f6c92ee66586c294945a
zstrad44 <zachstradling@gmail.com>
2016-12-04 15:29:07 -0700

    completed assignment 13

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'welcome/about'
 
+  get 'welcome/faq'
+
   root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
