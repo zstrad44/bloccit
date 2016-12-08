84e362be01fc07d4fad21e866a6923f7211e3b34
zstrad44 <zachstradling@gmail.com>
2016-12-07 17:38:53 -0700

    completed assignment-17

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,27 +5,45 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
+#
 require 'random_data'
 
 #Create Posts
-50.times do
-  Post.create!(
-    title: RandomData.random_sentence,
-    body: RandomData.random_paragraph
-  )
-end
-
-posts = Post.all
+# 50.times do
+#   Post.create!(
+#     title: RandomData.random_sentence,
+#     body: RandomData.random_paragraph
+#   )
+# end
+#
+# posts = Post.all
 
 #Create Comments
-100.times do
-  Comment.create!(
-    post: posts.sample,
-    body: RandomData.random_paragraph
-  )
+# 100.times do
+#   Comment.create!(
+#     post: posts.sample,
+#     body: RandomData.random_paragraph
+#   )
+# end
+
+# puts "#{Post.count} posts created"
+# puts "#{Comment.count} comments created"
+# puts "Seed finished"
+
+
+# ASSIGNMENT 17
+
+Post.find_or_create_by(title: "This post is very unique.") do |post|
+  post.body = "This is the body of the unique post."
 end
 
-puts "Seed finished"
+p = Post.find_by(title: "This post is very unique.")
+
+Comment.find_or_create_by(post: p) do |comment|
+  comment.body = "This comment is also unique."
+end
+
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+
+puts "Seed finished"
