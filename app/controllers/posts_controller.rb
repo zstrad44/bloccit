5e66ff17c01b154e26bed0640c1a82728ca44972
zstrad44 <zachstradling@gmail.com>
2016-12-07 23:03:26 -0700

    completed checkpoint 19

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,38 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.each do |post|
+      if post.id == 0 || (post.id%5) == 1
+        post.title = "SPAM"
+      end
+    end
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+      flash[:notice] = "Post was saved."
+      redirect_to @post
+    else
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render.new
+    end
   end
 
   def edit
   end
+
 end
+
+#The first post and every 5th post should have the title: spam
