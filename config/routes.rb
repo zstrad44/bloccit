aaa66d8823c892535b02d8377d2ec63fe91169d7
zstrad44 <zachstradling@gmail.com>
2016-12-10 15:08:00 -0700

    completed assignment 25

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   resources :topics do
     resources :posts, except: [:index]
   end
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :confirm]
 
+  post 'users/confirm' => 'users#confirm'
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
