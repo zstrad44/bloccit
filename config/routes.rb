3609c2bc5d174935093ce0f95a8193583e6c86df
zstrad44 <zachstradling@gmail.com>
2016-12-14 23:48:50 -0700

    completed checkpoint-29

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   resources :topics do
     resources :posts, except: [:index]
   end
+  #for comments
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
+  end
+
   resources :users, only: [:new, :create]
 
   resources :sessions, only: [:new, :create, :destroy]
