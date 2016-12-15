077418919513a5d5e8b725e410f1b20fc5ede596
zstrad44 <zachstradling@gmail.com>
2016-12-14 17:21:22 -0700

    completed assignment-27-scopes

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,13 @@ class Post < ActiveRecord::Base
 
   default_scope { order('created_at DESC') }
 
+  scope :ordered_by_title, -> { order('title DESC') }
+  scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
+
+  #Original Answer
+  #scope :ordered_by_title, -> {reorder(title: :asc)}
+  #scope :ordered_by_reverse_created_at, -> {reorder('created_at ASC')}
+
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
